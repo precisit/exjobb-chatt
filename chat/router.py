8aba5e6ffb108dcac4f092cf249e414433289054
Siri Persson <sirip@Magnuss-iMac-4.local>
2015-03-30 11:39:36 +0200

    Changes in router

--- chat/router.py
+++ chat/router.py
@@ -1,12 +1,16 @@
 import networkx as nx
 import json
 
-# The connection betweem the users and the topics are stored in g
+# The connection between the users and the topics are stored in connect
 # One node for each user and one node for each topic and one node for each socket
 # Users are accessed by getting neighbors of the userRootNode
 # Topics are accessed by getting neighbors of the topicRootNode
+connect=nx.Graph()
+connect.add_node('userRootNode')
+connect.add_node('topicRootNode')
 
-g=nx.Graph()
-g.add_node('userRootNode')
-g.add_node('topicRootNode')
+	def addConnection(socket): 
+		connect.add_node(socket)
+		socket.routing_key=None
 
+	def removeconnection(socket): 
