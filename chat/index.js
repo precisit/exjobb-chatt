6f5c54c8b4bc2245484d32c9ad7b05ddf4e5354e
Kerstin Ersson <kerstinersson@Kerstins-MacBook-Pro.local>
2015-03-31 12:51:39 +0200

    Unnecessary file index.js commented. Will be deleted later.

--- chat/index.js
+++ chat/index.js
@@ -1,12 +1,12 @@
-var app = require('express')();
+//var app = require('express')();
 // Express initializes app to be a function handler that you can supply to an HTTP server
-var http = require('http').Server(app);
+//var http = require('http').Server(app);
 
 // We define a route handler / that gets called when we hit our website home
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//app.get('/', function(req, res){
+//  res.sendFile(__dirname + '/index.html');
+//});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+//http.listen(3000, function(){
+//  console.log('listening on *:3000');
+//});
