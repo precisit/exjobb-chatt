31dc8943959c41eced1e08771df4402a02a95e28
Siri Persson <sirip@Magnuss-iMac-4.local>
2015-03-31 15:49:54 +0200

    Changes in messagehandling and server

--- chat/server.py
+++ chat/server.py
@@ -10,13 +10,13 @@ define("debug", default=False, help="run in debug mode")
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-	 	self.write("message")
+	 	self.write("Hello world")
 
 class EchoWebSocket(websocket.WebSocketHandler):
 	def open(self):
-		router.addConnection(self)
-		self.write_message('Welcome to Siris and Kerstins chat')
-		print "Websocket opened."
+		router.addConnection(self) # Connect to router
+		self.write_message('New client connected')
+		print "You are connected."
 
 	def on_message(self,message):
 		for client in clients
@@ -29,6 +29,8 @@ class EchoWebSocket(websocket.WebSocketHandler):
 	def check_origin(self,origin):
 		return True
 
+# r”” = regexp betyder att vi ska matcha
+# vilken klass ska hantera detta 
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/websocket", WebSocketHandler)
@@ -36,6 +38,7 @@ application = tornado.web.Application([
 def main():
     pika.log.setup(color=True)
  
+ 	# Get a handle to the instance of ioloop
     io_loop = tornado.ioloop.IOLoop.instance()
  
     # PikaClient is our rabbitmq consumer
@@ -43,8 +46,16 @@ def main():
     application.pc = pc
     application.pc.connect()
  
-    application.listen(3000)
-    io_loop.start()
+    application.listen(3000) # listen to the port
+    io_loop.start() # start the ioloop
 
+
+# By doing the main check, 
+# you can have that code only execute when you want to run the module as a program 
+# and not have it execute when someone just wants to import your module 
+# and call your functions themselves.
+# For example, if the python interpreter is running that module (the source file) as the main program, 
+# it sets the special __name__ variable to have a value "__main__". 
+# If this file is being imported from another module, __name__ will be set to the module's name.
 if __name__ == "__main__":
     main()
