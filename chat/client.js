edac6684cf23dc63331337c278a149c87daf7fc8
Kerstin Ersson <kerstinersson@Kerstins-MacBook-Pro.local>
2015-03-31 14:25:20 +0200

    Removed an unnecessary function from client.js

--- chat/client.js
+++ chat/client.js
@@ -8,23 +8,16 @@ var WebSocket = require('ws');
 var port= 3000;
 
 // Creates a websocket
-var ws = new WebSocket('ws://localhost:'+ port+'echoSocket')
+var ws = new WebSocket('ws://localhost:'+ port+'/echoSocket')
 
-// If the user has given another port the variable port is changed to the given number
-// process.argv is an array which contains the command line argument. 
-// The first element is 'node' 
-// The second element is the name of the JavaScript file
-// The next element (element number 2) will be any additional command line arguments
+/* If the user has given another port the variable port is changed to the given number
+process.argv is an array which contains the command line argument. 
+The first element is 'node' 
+The second element is the name of the JavaScript file
+The next element (element number 2) will be any additional command line arguments*/
 if (process.argv.length > 2) {
   port = process.argv[2];
 }
-
-
-
-
-
-
-
 
 // 2
 // Opens web socket and starts
@@ -33,15 +26,22 @@ if (process.argv.length > 2) {
 ws.on('open', function() {
 	console.log('Socket open!');
 	// Sending message
-	ws.send('Welcome to this excellent chat application!'); //Json representation skickas om object skall skickas
+	// ws.send('Welcome to this excellent chat application!'); //Json representation skickas om object skall skickas
 });
 
+// ensures that interval timers arent still running after the
+// client has disconnected
+ws.on('close', function() {
+  console.log('Disconnected');
+  clearInterval(id);
+  exit();
+});
 
-
-
-
-
-
+ws.on('message', function(data, flags) {
+  // flags.binary will be set if binary data is received
+  // flags.masked will be set if the data was masked
+  console.log(data);
+});
 
 // 3
 // Client input 
@@ -75,57 +75,42 @@ ws.on('open', function() {
     return 1;
   }
 
-
-
-
-
-
-
-
 // 4
 // Sending and receiving messages
 
-// By defining an onopen handler attempting to send data ONLY takes place once a connection is established 
+// By defining an onopen handler attempting to send data ONLY 
+// takes place once a connection is established 
 ws.onopen = function (event) {
   ws.send("Here's some text that the server is urgently awaiting!"); 
 };
-
-ws.on('message', function(data, flags) {
-	console.log('Server says', data);
-});
 
 // Receiving messages from the server
 ws.onmessage = function (event) {
   console.log(event.data);
 }
 
-// Send text to all users through the server
-function sendText() {
-  // Construct a msg object containing the data the server needs to process the message from the chat client.
-  var msg = {
-    type: "message",
-    text: document.getElementById("text").value,
-    id:   clientID,
-    date: Date.now()
-  };
-  // Send the msg object as a JSON-formatted string.
-  ws.send(JSON.stringify(msg));
-  // Blank the text input element, ready to receive the next line of text from the user.
-  document.getElementById("text").value = "";
-}
-
-
-
-
-
-
-
 // 5
 // Exiting and closing 
-
 function exit() {
 ws.close(); // Close the websocket connection 
 console.log('Socket closed!');
 console.log('Goodbye!');
 process.exit(); // Exit
 }
+
+
+// BEHÖVS DETTA?
+// Send text to all users through the server
+// function sendText() {
+  // Construct a msg object containing the data the server needs to process the message from the chat client.
+//  var msg = {
+//    type: "message",
+//    text: document.getElementById("text").value,
+//    id:   clientID,
+//    date: Date.now()
+//  };
+//  // Send the msg object as a JSON-formatted string.
+//  ws.send(JSON.stringify(msg));
+  // Blank the text input element, ready to receive the next line of text from the user.
+//  document.getElementById("text").value = "";
+//}
