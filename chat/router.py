e535c1059d77aa3137334c2f5d2b5064914dddb6
Siri Persson <sirip@Magnuss-iMac-4.local>
2015-04-10 14:57:55 +0200

    Changes in raouter.py, networkx

--- chat/router.py
+++ chat/router.py
@@ -1,20 +1,35 @@
 import json
+import networkx as nx
+
+userNode='userNode'
+topicNode='topicNode'
+
+# Create an empty graph with no nodes and no edges 
+g = nx.Graph()
+# Add the nodes 
+g.add_node(userNode)
+g.add_node(topicNode)
+
+
+
+# Här börjar vår gamla kod 
+
 
 # store connections
-clients = []
+#clients = []
 # store usernames
-users = []
+#users = []
 
 # get user name
-def getUserName(socket):
-	i = clients.index(socket)
+#def getUserName(socket):
+#	i = clients.index(socket)
 
-	return users[i]
+#	return users[i]
 
 # set user name
-def setUserName(socket, newUserName):
-	newUserName = str(newUserName)
-	userName = newUserName
+#def setUserName(socket, newUserName):
+#	newUserName = str(newUserName)
+#	userName = newUserName
 
 #	if userName == '':
 #		userName = getUserName(socket)
@@ -25,65 +40,72 @@ def setUserName(socket, newUserName):
 #	else:
 #		socket.write_message('Your username is: ', userName)
 
-	i = clients.index(socket)
-	users.append(userName)
-	socket.write_message("Your username is " + userName)
+#	i = clients.index(socket)
+#	users.append(userName)
+#	socket.write_message("Your username is " + userName)
 
 # handle message
-def handleMessage(socket, message):
+#def handleMessage(socket, message):
 	#print "Message: %s" % message
-	usrandmessage = message.partition(" ")
+#	usrandmessage = message.partition(" ")
 
-	if (usrandmessage[0] == "usr"):
-		usr = usrandmessage[2]
-		setUserName(socket, usr)
+#	if (usrandmessage[0] == "usr"):
+#		usr = usrandmessage[2]
+#		setUserName(socket, usr)
 
 	#elif (usrandmessage[0] == "exit"):
 	#	removeConnection(socket)
 
-	else:
-		if (len(users) == 0):
-			socket.write_message("Set a username first!")
-			return
+#	else:
+#		if (len(users) == 0):
+#			socket.write_message("Set a username first!")
+#			return
 
-		userName = getUserName(socket)
-		if userName is None:
-			socket.write_message("Set a username first!")
-			return
+#		userName = getUserName(socket)
+#		if userName is None:
+#			socket.write_message("Set a username first!")
+#			return
 
-		message = {
-			'user': userName,
-			'body': message
-		}
+#		message = {
+#			'user': userName,
+#			'body': message
+#		}
 
-		print "Send message"
-		routing_key = socket.routing_key
-		sendMessage = json.dumps(message)
+#		print "Send message"
+#		routing_key = socket.routing_key
+#		sendMessage = json.dumps(message)
 
 		#print sendMessage
-		pc.send_message(routing_key, sendMessage)
+#		pc.send_message(routing_key, sendMessage)
 
-def processMessage(routing_key, message):
-	ind=[]
-	for socket in clients:
-		if socket.routing_key == routing_key:
-			ind.append(clients.index(socket))
+#def processMessage(routing_key, message):
+#	ind=[]
+#	for socket in clients:
+#		if socket.routing_key == routing_key:
+#			ind.append(clients.index(socket))
 
-	for i in range(0,len(ind)):
-		s = clients[ind[i]] 
-		data = dict(json.loads(message))
+#	for i in range(0,len(ind)):
+#		s = clients[ind[i]] 
+#		data = dict(json.loads(message))
 
-		s.write_message("%s says %s" % (data['user'], data['body']))
+#		s.write_message("%s says %s" % (data['user'], data['body']))
 
 # add connection
-def addConnection(socket):
-	clients.append(socket)
-	print clients
-	socket.routing_key = 'routing_key'
+#def addConnection(socket):
+#	clients.append(socket)
+#	print clients
+#	socket.routing_key = 'routing_key'
+g.add_node(socket)
+
 
 # remove connection
-def removeConnection(socket):
-	i = clients.index(socket)
-	clients.remove(socket)
+#def removeConnection(socket):
+#	i = clients.index(socket)
+#	clients.remove(socket)
+# Remove username node
+	
+	# Remove socket node
+	g.remove_node(socket)
+
 
 	#del clients[i]
